c5a9616b0713a33d1814b6533ee818dd72fca633
Parlee Khadka <khadkaparlee@gmail.com>
2025-06-14 09:56:58 +0545

    Refactor for Vercel deployment without e

--- api/getData.js
+++
@@ -1,9 +0,0 @@
-const fetchData = require('../services');
-
-module.exports = async (req, res) => {
-  const word = req.query.word;
-  console.log(word);
-  const response = await fetchData(word);
-  console.log(response);
-  res.status(200).json({ data: response });
-};
